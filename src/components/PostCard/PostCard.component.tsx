78289f4c42a2bf5e3809944f399710424d2af869
Kristian Sørum <kristian.sorum@no.experis.com>
2022-10-10 12:06:13 +0200

    dashboard layout

--- src/components/PostCard/PostCard.component.tsx
+++ src/components/PostCard/PostCard.component.tsx
@@ -5,7 +5,7 @@ const PostCard = () => {
     <div className={styles.card}>
       <p className={styles.postedInfo}>Posted in *Group* by *User*</p>
       <p className={styles.title}>Title</p>
-      <div className={styles.postBody}>Post Body</div>
+      <div className={styles.postBody}></div>
       <p className={styles.comments}>42 comments</p>
     </div>
   );
--- src/views/DashboardView/DashboardView.tsx
+++ src/views/DashboardView/DashboardView.tsx
@@ -2,17 +2,20 @@ import TimelineComponent from "../../components/Timeline/Timeline.component";
 import ListBox from "../../components/ListBox/ListBox.component";
 import ProfileCard from "../../components/ProfileCard/ProfileCard.component";
 import FiltersCard from "../../components/FiltersCard/FiltersCard.component";
+import styles from "./DashboardView.module.css";
 
 const DashboardView = () => {
   return (
-    <div>
-      <div>
+    <div className={styles.dashboard}>
+      <div className={styles.groupsTopicsEventsListsColumn}>
         <ListBox />
         <ListBox />
         <ListBox />
       </div>
-      <TimelineComponent />
-      <div>
+      <div className={styles.timelineColumn}>
+        <TimelineComponent />
+      </div>
+      <div className={styles.profileAndFilterColumn}>
         <ProfileCard />
         <FiltersCard />
       </div>
