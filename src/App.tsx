2c543c333d9446854579ebde6cb59a4833be1da2
Kristian Sørum <kristian.sorum@no.experis.com>
2022-10-10 17:48:55 +0200

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./Global.css";
 import DashboardView from "./views/DashboardView/DashboardView";
 import styles from "./App.module.css";
+import NavBar from "./components/NavBar/NavBar.component";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <div className={styles.dashboard}>
         <DashboardView />
       </div>
--- src/components/NavBar/NavBar.component.tsx
+++ src/components/NavBar/NavBar.component.tsx
@@ -1,5 +1,19 @@
+import styles from "./NavBar.module.css";
+import ProfilePic from "../ProfilePic/ProfilePic.component";
+import Logo from "../Logo/Logo.component";
+
 const NavBar = () => {
-  return <div></div>;
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.logo}>
+        <Logo />
+      </div>
+      <input type="text" placeholder="Search" />
+      <div className={styles.profile}>
+        <ProfilePic />
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
