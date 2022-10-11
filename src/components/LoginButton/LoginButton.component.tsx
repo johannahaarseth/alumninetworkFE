284cb2643ef307aba32480d2e705054bd9687e56
“fvlinder519” <fanni.finheim@no.experis.com>
2022-10-11 11:57:05 +0200

    button and text

---
+++ src/components/LoginButton/LoginButton.component.tsx
@@ -0,0 +1,11 @@
+import styles from "./LoginButton.module.css";
+
+const LoginButton = () => {
+  return (
+    <>
+      <div className={styles.button}>Log In</div>
+    </>
+  );
+};
+
+export default LoginButton;
--- src/components/LoginCard/LoginCard.component.tsx
+++ src/components/LoginCard/LoginCard.component.tsx
@@ -1,7 +1,15 @@
+import LoginButton from "../LoginButton/LoginButton.component";
 import styles from "./LoginCard.module.css";
 
 const LoginCard = () => {
-  return <div className={styles.card}></div>;
+  return (
+    <>
+      <div className={styles.card}>
+        <h2 className={styles.h2}>Welcome!</h2>
+        <LoginButton />
+      </div>
+    </>
+  );
 };
 
 export default LoginCard;
