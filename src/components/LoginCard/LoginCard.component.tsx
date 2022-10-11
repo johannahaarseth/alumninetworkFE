4a311c73c72d4901bb5f7a6a3449c380fd62f09a
“fvlinder519” <fanni.finheim@no.experis.com>
2022-10-11 15:25:01 +0200

    farge endinrg

--- src/components/LoginCard/LoginCard.component.tsx
+++ src/components/LoginCard/LoginCard.component.tsx
@@ -4,14 +4,14 @@ import styles from "./LoginCard.module.css";
 const LoginCard = () => {
   const logo = require("../../assets/Pictures/LogoAlumni.png");
   const illustration = require("../../assets/Pictures/whiteIllustration.jpg");
+
   return (
     <>
       <div className={styles.card}>
-        <img className={styles.illustration} src={illustration} />
-
+        <img alt="" className={styles.illustration} src={illustration} />
         <h1 className={styles.title}>Welcome back!</h1>
         <h2 className={styles.undertitle}>Sign in to gain access.</h2>
-        <img className={styles.logo} src={logo} />
+        <img alt="" className={styles.logo} src={logo} />
         <LoginButton />
       </div>
     </>
