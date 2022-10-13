96d87a85776dd5ec37d24d84a64870598e2d74cf
“fvlinder519” <fanni.finheim@no.experis.com>
2022-10-13 15:26:23 +0200

    show profilecard on profile

--- src/views/ProfileView/ProfileView.tsx
+++ src/views/ProfileView/ProfileView.tsx
@@ -1,7 +1,22 @@
+import FiltersCard from "../../components/FiltersCard/FiltersCard.component";
 import NavBar from "../../components/NavBar/NavBar.component";
+import ProfileCard from "../../components/ProfileCard/ProfileCard.component";
+import React from "react";
+import profile from "../../../src/components/ProfileCard/ProfileCard.module.css";
+import styles from "../DashboardView/DashboardView.module.css";
 
 const ProfileView = () => {
-  return <NavBar />;
+  return (
+    <>
+      <NavBar />
+      <div className={styles.dashboard}>
+        <div className={profile.profileAndFilterColumn}>
+          <ProfileCard />
+          <FiltersCard />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ProfileView;
