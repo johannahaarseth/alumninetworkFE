1aeb360f8adc04a4b1a73532a373a0dce0182824
“fvlinder519” <fanni.finheim@no.experis.com>
2022-10-13 12:34:30 +0200

    fixing code after comments on azure

--- src/components/LoginButton/LoginButton.component.tsx
+++ src/components/LoginButton/LoginButton.component.tsx
@@ -1,13 +1,5 @@
 import styles from "./LoginButton.module.css";
-
-//import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = () => {
-  //const { loginWithPopup, user } = useAuth0();
-
-  //   const login = () => {
-  //     loginWithPopup().then(() => console.log("Logger inn"));
-  //   };
-
   return <button className={styles.button}>Log In</button>;
 };
 
