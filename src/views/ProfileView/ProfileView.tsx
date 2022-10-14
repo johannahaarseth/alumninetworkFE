838800625c86ba32f9ca329cb3befd2530ba6bf2
Kristian Sørum <kristian.sorum@no.experis.com>
2022-10-14 09:12:40 +0200

    css for ProfileView

--- src/views/ProfileView/ProfileView.tsx
+++ src/views/ProfileView/ProfileView.tsx
@@ -2,17 +2,20 @@ import FiltersCard from "../../components/FiltersCard/FiltersCard.component";
 import NavBar from "../../components/NavBar/NavBar.component";
 import ProfileCard from "../../components/ProfileCard/ProfileCard.component";
 import React from "react";
-import profile from "../../../src/components/ProfileCard/ProfileCard.module.css";
-import styles from "../DashboardView/DashboardView.module.css";
+import styles from "./ProfileView.module.css";
 
 const ProfileView = () => {
   return (
     <>
       <NavBar />
-      <div className={styles.dashboard}>
-        <div className={profile.profileAndFilterColumn}>
-          <ProfileCard />
-          <FiltersCard />
+      <div className={styles.container}>
+        <div className={styles.profileView}>
+          <div className={styles.emptyColumn}></div>
+          <div className={styles.postsColumn}></div>
+          <div className={styles.profileAndFilterColumn}>
+            <ProfileCard />
+            <FiltersCard />
+          </div>
         </div>
       </div>
     </>
