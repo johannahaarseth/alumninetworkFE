1622868cc4d8f3d06cc00bfc9848aa4f01b1e03d
Kristian Sørum <kristian.sorum@no.experis.com>
2022-10-14 09:29:54 +0200

    fix for at profilbildet fløt over navbar

--- src/components/ProfilePic/ProfilePic.component.tsx
+++ src/components/ProfilePic/ProfilePic.component.tsx
@@ -1,19 +1,11 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import styles from "./ProfilePic.module.css";
-import Avatar from "@mui/material/Avatar";
 
 const ProfilePic = () => {
   const { user } = useAuth0();
   return (
     <div className={styles.profilePic}>
-      <Avatar
-        src={user?.picture}
-        alt={user?.name}
-        sx={{
-          height: "100%",
-          width: "100%",
-        }}
-      />
+      <img src={user?.picture} alt={user?.name} />
     </div>
   );
 };
