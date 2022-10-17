c7db28ee634b691b9b3a3802de244d8b18cf8d7f
Johanna Haarseth <johanna.haarseth@np.experis.com>
2022-10-17 07:22:13 +0200

    Add key

--- src/components/Timeline/Timeline.component.tsx
+++ src/components/Timeline/Timeline.component.tsx
@@ -8,9 +8,8 @@ type TimelineProps = {
 };
 
 const Timeline = ({posts} : TimelineProps) => {
-
   const timelineContent = posts?.map((post, i) => {
-    return <PostCard post={post} />;
+    return <PostCard post={post} key={i}/>;
   });
 
   return <div>{timelineContent}</div>;
