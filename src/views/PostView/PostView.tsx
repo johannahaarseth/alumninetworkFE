64f5ef08a9880683fc9e0706fd99a7cfad29653a
“fvlinder519” <fanni.finheim@no.experis.com>
2022-10-17 16:22:46 +0200

    start of modal

--- src/views/PostView/PostView.tsx
+++ src/views/PostView/PostView.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import Button from "../../components/Button/Button.component";
 import CreateNewPost from "../../components/CreateNewPost/CreateNewPost.component";
 import EventCard from "../../components/EventCard/EventCard.component";
 import GroupCard from "../../components/GroupCard/GroupCard.component";
 import NavBar from "../../components/NavBar/NavBar.component";
+import RadioButton from "../../components/RadioButton/RadioButton.component";
 import TimelineComponent from "../../components/Timeline/Timeline.component";
 import styles from "./PostView.module.css";
+import Modal from "../../components/Modal/Modal.component";
+import { LocalizationProvider, DateTimePicker } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { locale } from "dayjs";
+import { useNavigate } from "react-router-dom";
+import Input from "../../components/Input/Input.component";
 
+import TextField1 from "../../components/TextField/TextField.component";
 const PostView = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
   return (
     <>
       <NavBar />
@@ -18,7 +29,6 @@ const PostView = () => {
             <CreateNewPost />
             <TimelineComponent />
           </div>
-
           <div className={styles.profile}>
             {window.location.pathname === "/event" ? (
               <EventCard />
@@ -26,7 +36,7 @@ const PostView = () => {
               <GroupCard />
             )}
 
-            <Button className={styles.button}>
+            <Button className={styles.button} onClick={() => setIsOpen(true)}>
               {window.location.pathname === "/topic" ? (
                 <p className={styles.newPost}>Join</p>
               ) : (
@@ -37,6 +47,20 @@ const PostView = () => {
           </div>
         </div>
       </div>
+      {isOpen && (
+        <Modal setIsOpen={setIsOpen}>
+          <p className={styles.modalHeader}>Group title</p>
+          <form className={styles.form}>
+            <div>
+              <Input placeholderText={`Search: name`} />
+            </div>
+
+            <div>
+              <TextField1 placeholderText={`Add description`} />
+            </div>
+          </form>
+        </Modal>
+      )}
     </>
   );
 };
