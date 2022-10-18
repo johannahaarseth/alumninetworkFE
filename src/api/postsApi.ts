198073b8ec13e48de5790e62809e0e58e59d3fd8
Kristian Sørum <kristian.sorum@no.experis.com>
2022-10-18 17:14:33 +0200

    cleanup

--- src/api/postsApi.ts
+++ src/api/postsApi.ts
@@ -1,18 +1,15 @@
-import { IPostResponse } from './../interfaces/IPostResponse';
-import {apiClient} from "./apiClient";
+import { IPostResponse } from "../interfaces/IPostResponse";
+import { apiClient } from "./apiClient";
 
 const config = {
   headers: {
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
-  }
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  },
 };
 
 const getPosts = () => apiClient.get<IPostResponse>("/post", config);
-const getPostById = (postId: number) => apiClient.get<IPostResponse>("/post/" + postId);
+const getPostById = (postId: number) =>
+  apiClient.get<IPostResponse>("/post/" + postId);
 
-
-export {
-  getPosts,
-  getPostById
-};
+export { getPosts, getPostById };
