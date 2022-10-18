ba5d11cad77a475b3d2d62e223a72a136c210eaa
“fvlinder519” <fanni.finheim@no.experis.com>
2022-10-18 09:12:46 +0200

    menustart

--- src/components/NavBar/NavBar.component.tsx
+++ src/components/NavBar/NavBar.component.tsx
@@ -7,12 +7,38 @@ import NavBarProfileOnClickCard from "../NavBarProfileOnClickCard/NavBarProfileO
 import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
-const NavBar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  Avatar,
+  Box,
+  Button,
+  IconButton,
+  MenuItem,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 
+const NavBar = () => {
   const { isAuthenticated } = useAuth0();
   const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
   return (
     <>
       <div className={styles.navbar}>
@@ -29,8 +55,43 @@ const NavBar = () => {
         <div className={styles.profile} onClick={() => setIsOpen(true)}>
           <ProfilePic />
         </div>
+        {isOpen && <NavBarProfileOnClickCard setIsOpen={setIsOpen} />}
+
+        <div className={styles.hamburger}>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton
+                onClick={handleOpenUserMenu}
+                sx={{ p: 1, ml: "50px" }}
+              >
+                <MenuIcon />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              <MenuItem onClick={handleCloseUserMenu} sx={{ width: "100%" }}>
+                <Button onClick={() => navigate("/profile")}>
+                  <Typography textAlign="center">Profile</Typography>
+                </Button>
+              </MenuItem>
+            </Menu>
+          </Box>
+        </div>
       </div>
-      {isOpen && <NavBarProfileOnClickCard setIsOpen={setIsOpen} />}
     </>
   );
 };
