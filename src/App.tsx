9207f785748e9374ad892f7af97193dbf638fa9b
Kristian Sørum <kristian.sorum@no.experis.com>
2022-10-18 16:59:22 +0200

    fjernet context api, modeller/interfaces og dummydata. Måtte endre på url path igjen (sorry Fanni)

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import DashboardView from "./views/DashboardView/DashboardView";
 import ProfileView from "./views/ProfileView/ProfileView";
 import { useAuth0 } from "@auth0/auth0-react";
 import ListView from "./views/ListView/ListView";
-import AppProvider from "./context/AppProvider";
 import PostView from "./views/PostView/PostView";
 
 function App() {
@@ -13,40 +12,46 @@ function App() {
 
   return !isLoading ? (
     <div className="App">
-      <AppProvider>
-        <BrowserRouter>
-          <div className="App">
-            <Routes>
-              <Route
-                path="/login"
-                element={
-                  isAuthenticated ? <Navigate to="/dashboard" /> : <LoginView />
-                }
-              />
-              <Route
-                path="*"
-                element={
-                  isAuthenticated ? <Navigate to="/dashboard" /> : <LoginView />
-                }
-              />
-              {isAuthenticated && (
-                <Route path="/dashboard" element={<DashboardView />} />
-              )}
-              {isAuthenticated && (
-                <Route path="/profile" element={<ProfileView />} />
-              )}
-                {/* change afterwards to => path="/group/:id/" */}
-                {isAuthenticated && <Route path="/group" element={<PostView />} />}
-                {/* change afterwards to => path="/topic/:id/" */}
-                {isAuthenticated && <Route path="/topic" element={<PostView />} />}
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route
+              path="/login"
+              element={
+                isAuthenticated ? <Navigate to="/dashboard" /> : <LoginView />
+              }
+            />
+            <Route
+              path="*"
+              element={
+                isAuthenticated ? <Navigate to="/dashboard" /> : <LoginView />
+              }
+            />
+            {isAuthenticated && (
+              <Route path="/dashboard" element={<DashboardView />} />
+            )}
+            {isAuthenticated && (
+              <Route path="/profile" element={<ProfileView />} />
+            )}
+            {/* change afterwards to => path="/group/:id/" */}
+            {isAuthenticated && <Route path="/group" element={<PostView />} />}
+            {/* change afterwards to => path="/topic/:id/" */}
+            {isAuthenticated && <Route path="/topic" element={<PostView />} />}
 
-                {/* change afterwards to => path="/event/:id/" */}
-                {isAuthenticated && <Route path="/event" element={<PostView />} />}
-              {isAuthenticated && <Route path="/list" element={<ListView />} />}
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </AppProvider>
+            {/* change afterwards to => path="/event/:id/" */}
+            {isAuthenticated && <Route path="/event" element={<PostView />} />}
+            {isAuthenticated && (
+              <Route path="/groups" element={<ListView title="Groups" />} />
+            )}
+            {isAuthenticated && (
+              <Route path="/topics" element={<ListView title="Topics" />} />
+            )}
+            {isAuthenticated && (
+              <Route path="/events" element={<ListView title="Events" />} />
+            )}
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   ) : (
     <>
