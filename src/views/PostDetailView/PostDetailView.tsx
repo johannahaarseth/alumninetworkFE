6a119bed68ec92fb7ff0650fdce57b94749691f2
Kristian Sørum <kristian.sorum@no.experis.com>
2022-10-19 11:02:46 +0200

    post detail view

--- src/views/PostDetailView/PostDetailView.tsx
+++ src/views/PostDetailView/PostDetailView.tsx
@@ -1,5 +1,48 @@
+import NavBar from "../../components/NavBar/NavBar.component";
+import styles from "./PostDetailView.module.css";
+import EventCard from "../../components/EventCard/EventCard.component";
+import GroupCard from "../../components/GroupCard/GroupCard.component";
+import Button from "../../components/Button/Button.component";
+import PostEventCard from "../../components/PostEventCard/PostEventCard.component";
+import Card from "../../components/Card/Card.component";
+import ButtonCustom from "../../components/ButtonCustom/ButtonCustom";
+import Input from "../../components/Input/Input.component";
+
 const PostDetailView = () => {
-  return <div></div>;
+  return (
+    <>
+      <NavBar />
+      <div className={styles.container}>
+        <div className={styles.postView}>
+          <div className={styles.emptyColumn}></div>
+          <div className={styles.postColumn}>
+            <PostEventCard />
+            <div className={styles.commentsContainer}>
+              <Card cardHoverEffect={true}>
+                <p>This is a comment</p>
+              </Card>
+              <Card cardHoverEffect={true}>
+                <p>This is another comment</p>
+              </Card>
+            </div>
+            <hr />
+            <div className={styles.postCommentContainer}>
+              <Input placeholderText="Type a comment..." />
+              <Button className={styles.replyBtn}>Reply</Button>
+            </div>
+          </div>
+          <div className={styles.infoColumn}>
+            {window.location.pathname.match(/event/) ? (
+              <EventCard />
+            ) : (
+              <GroupCard />
+            )}
+            <ButtonCustom />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default PostDetailView;
