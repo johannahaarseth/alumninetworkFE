9ecf01e89a7e63ebad618b31054b70f8ae1c7ebf
Kristian Sørum <kristian.sorum@no.experis.com>
2022-10-20 08:47:38 +0200

    modal fix

--- src/components/CreateNewPost/CreateNewPost.component.tsx
+++ src/components/CreateNewPost/CreateNewPost.component.tsx
@@ -1,7 +1,7 @@
 import styles from "./CreateNewPost.module.css";
 import Card from "../Card/Card.component";
 import { useState } from "react";
-import Modal from "../Modal/Modal.component";
+import Modal from "@mui/material/Modal";
 import Input from "../Input/Input.component";
 import RadioButton from "../RadioButton/RadioButton.component";
 import TextField from "../TextField/TextField.component";
@@ -9,41 +9,48 @@ import Button from "../Button/Button.component";
 import { useNavigate } from "react-router-dom";
 
 const CreateNewPost = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   const navigate = useNavigate();
 
   return (
     <>
       <Card cardHoverEffect={true}>
-        <button
-          className={styles.createNewPostButton}
-          onClick={() => setIsOpen(true)}
-        >
+        <button className={styles.createNewPostButton} onClick={handleOpen}>
           <p className={styles.newPost}>Create new post</p>
           <p className={styles.newPostPlus}>+</p>
         </button>
       </Card>
-      {isOpen && (
-        <Modal setIsOpen={setIsOpen}>
-          <p className={styles.modalHeader}>Create new post</p>
-          <form className={styles.form}>
-            <Input placeholderText={"Add post title"} />
-            <p>Post to:</p>
-            <div className={styles.radioButtons}>
-              <RadioButton valueProp={"Group"} />
-              <RadioButton valueProp={"Event"} />
-              <RadioButton valueProp={"Person"} />
+      {open && (
+        <Modal open={open} onClose={handleClose}>
+          <div className={styles.centered}>
+            <div className={styles.modal}>
+              <Card cardHoverEffect={false}>
+                <p className={styles.modalHeader}>Create new post</p>
+                <form className={styles.form}>
+                  <Input placeholderText={"Add post title"} />
+                  <p>Post to:</p>
+                  <div className={styles.radioButtons}>
+                    <RadioButton valueProp={"Group"} />
+                    <RadioButton valueProp={"Event"} />
+                    <RadioButton valueProp={"Person"} />
+                  </div>
+                  <Input
+                    placeholderText={"Search for group / event / person"}
+                  />
+                  <div>
+                    <TextField placeholderText={"Add post content"} />
+                  </div>
+                  <div className={styles.buttonContainer}>
+                    <Button onClick={() => navigate("/group")}>
+                      <p>Create post &gt;</p>
+                    </Button>
+                  </div>
+                </form>
+              </Card>
             </div>
-            <Input placeholderText={"Search for group / event / person"} />
-            <div>
-              <TextField placeholderText={"Add post content"} />
-            </div>
-            <div className={styles.buttonContainer}>
-              <Button onClick={() => navigate("/group")}>
-                <p>Create post &gt;</p>
-              </Button>
-            </div>
-          </form>
+          </div>
         </Modal>
       )}
     </>
