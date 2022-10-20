65f207b9bbbff45f03530897457ec0632f48dcce
Johanna Haarseth <johanna.haarseth@np.experis.com>
2022-10-20 11:16:50 +0200

    Fjerner unødvendig endring

--- src/components/ProfilePic/ProfilePic.component.tsx
+++ src/components/ProfilePic/ProfilePic.component.tsx
@@ -3,7 +3,6 @@ import styles from "./ProfilePic.module.css";
 
 const ProfilePic = () => {
 	const { user } = useAuth0();
-
 	return (
 		<div className={styles.profilePic}>
 			<img src={user?.picture} alt={user?.name} />
