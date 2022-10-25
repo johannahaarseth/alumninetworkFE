e8c88f73bf222b4b99eb7168444b94d83444670e
“fvlinder519” <fanni.finheim@no.experis.com>
2022-10-25 11:01:12 +0200

    IPostGroup

--- src/components/CreateNewPost/CreateNewPost.component.tsx
+++ src/components/CreateNewPost/CreateNewPost.component.tsx
@@ -1,18 +1,52 @@
 import styles from "./CreateNewPost.module.css";
 import Card from "../Card/Card.component";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "@mui/material/Modal";
 import Input from "../Input/Input.component";
 import RadioButton from "../RadioButton/RadioButton.component";
 import TextField from "../TextField/TextField.component";
 import Button from "../Button/Button.component";
 import { useNavigate } from "react-router-dom";
+import { apiClient } from "../../api/apiClient";
+import { useApi } from "../../api/useApi";
+import { IEventResponse } from "../../interfaces/IEventResponse";
+import { IGroupResponse } from "../../interfaces/IGroupResponse";
+import { ITopicResponse } from "../../interfaces/ITopicResponse";
+import { IPostResponse } from "../../interfaces/IPostResponse";
 
 const CreateNewPost = () => {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const navigate = useNavigate();
+
+  const getGroupApi = useApi<IGroupResponse>(
+    (config: {}) =>
+      apiClient.get<IGroupResponse>("/group?offset=0&limit=3", config),
+    {} as IGroupResponse
+  );
+  const getTopicApi = useApi<ITopicResponse>(
+    (config: {}) =>
+      apiClient.get<ITopicResponse>("/topic?offset=0&limit=3", config),
+    {} as ITopicResponse
+  );
+  const getEventApi = useApi<IEventResponse>(
+    (config: {}) =>
+      apiClient.get<IEventResponse>("/event?offset=0&limit=3", config),
+    {} as IEventResponse
+  );
+  useEffect(() => {
+    getGroupApi.request().then();
+    getTopicApi.request().then();
+    getEventApi.request().then();
+    // eslint-disable-next-line
+  }, []);
+
+  const [posts, setPosts] = useState<IPostResponse>({
+    count: 0,
+    next: "",
+    results: [],
+  } as IPostResponse);
 
   return (
     <>
---
+++ src/interfaces/IPostGroup.ts
@@ -0,0 +1,9 @@
+import { ITarget } from "./ITarget";
+
+export interface IPostGroup {
+  targetGroup: ITarget;
+  topicId: number;
+  groupId: number;
+  postTitle: string;
+  postBody: string;
+}
